d23c70248817d3f11d6acaa47d23246fe523d2a2
Midgar <80575182+JesseCaddell@users.noreply.github.com>
2024-02-15 20:55:40 -0800

    gatsby, no optional

---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Project = ({ title, description, imageUrl }) => (
+    <div className="project">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <img src={imageUrl} alt={title}/>
+    </div>
+);
+
+export default Project;
